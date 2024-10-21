776b7cc4c8399705aa240a9236a82d87f58f34af
Tim Moss <tmoss726@hotmail.com>
2024-10-20 21:52:50 -0400

    Gravity added for player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,20 @@
 extends CharacterBody2D
+
+@export var gravity = 400
+@export var speed = 125
+@export var jump_force = 200
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	if is_on_floor() == false:
+		velocity.y += gravity * delta
+		if velocity.y > 500:
+			velocity.y = 500
+		
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force
+	
+	var direction =	Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	
+	move_and_slide()
