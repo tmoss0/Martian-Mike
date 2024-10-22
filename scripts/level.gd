387df6e83b5af4a0228bd880782f02ceffd91681
Tim Moss <tmoss726@hotmail.com>
2024-10-21 20:44:38 -0400

    Add deathzone when falling off the level

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var start_position = $StartPosition
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,3 +12,7 @@ func _process(delta: float) -> void:
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+
+func _on_deathzone_body_entered(body: Node2D) -> void:
+	body.velocity = Vector2.ZERO
+	body.global_position = start_position.global_position
