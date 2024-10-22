58ac64945d5105bc21d32699e0387f80c0d84091
Tim Moss <tmoss726@hotmail.com>
2024-10-21 20:19:54 -0400

    Animated sprites attached to character

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,10 +11,26 @@ func _physics_process(delta):
 		if velocity.y > 500:
 			velocity.y = 500
 
-	if Input.is_action_just_pressed("jump") && is_on_floor():
+	if Input.is_action_just_pressed("jump"): # && is_on_floor():
 		velocity.y = -jump_force
 	
 	var direction =	Input.get_axis("move_left", "move_right")
-	velocity.x = direction * speed
+	if direction != 0:
+		animated_sprite.flip_h = (direction == -1)
 	
+	velocity.x = direction * speed	
 	move_and_slide()
+	
+	update_animations(direction)
+	
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else: 
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else: 
+			animated_sprite.play("fall")
