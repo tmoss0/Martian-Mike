ee9e4e6798b2b316bfbe2794e4c0b3f2bf31625b
Tim Moss <tmoss726@hotmail.com>
2024-10-22 22:35:06 -0400

    Connect groups to traps using for loop

--- scripts/level.gd
+++ scripts/level.gd
@@ -5,6 +5,8 @@ extends Node2D
 
 func _ready() -> void:
 	var traps = get_tree().get_nodes_in_group("traps")
+	for trap in traps:
+		trap.touched_player.connect(_on_trap_touched_player)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
