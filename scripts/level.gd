bcd31bb59a8acc97004eede5905680ce03d80f93
Tim Moss <tmoss726@hotmail.com>
2024-10-23 23:22:25 -0400

    Added start position for the player

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
-@onready var start_position = $StartPosition
-@onready var player = $Player
+@onready var start = $Start
+var player = null
 
 func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+	if player != null:
+		player.global_position = start.get_spawn_pos()
 	var traps = get_tree().get_nodes_in_group("traps")
 	for trap in traps:
 		trap.touched_player.connect(_on_trap_touched_player)
@@ -23,4 +26,4 @@ func _on_trap_touched_player() -> void:
 	
 func reset_player():
 	player.velocity = Vector2.ZERO
-	player.global_position = start_position.global_position
+	player.global_position = start.get_spawn_pos()
