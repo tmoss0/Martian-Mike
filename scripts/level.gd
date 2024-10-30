bd20737f92a4bb1c7704d46c065260d44bbf2176
Tim Moss <tmoss726@hotmail.com>
2024-10-29 22:21:11 -0400

    Finish Godot course

--- scripts/level.gd
+++ scripts/level.gd
@@ -46,9 +46,11 @@ func _process(_delta: float) -> void:
 		get_tree().reload_current_scene()
 
 func _on_deathzone_body_entered(_body: Node2D) -> void:
+	AudioPlayer.play_sfx("hurt")
 	reset_player()
 
 func _on_trap_touched_player() -> void:
+	AudioPlayer.play_sfx("hurt")
 	reset_player()
 	
 func _on_exit_body_entered(body) -> void:
@@ -68,6 +70,7 @@ func _on_level_timer_timeout():
 		time_left -= 1
 		hud.set_time_label(time_left)
 		if time_left < 0:
+			AudioPlayer.play_sfx("hurt")
 			reset_player()
 			time_left = level_time
 			hud.set_time_label(time_left)
